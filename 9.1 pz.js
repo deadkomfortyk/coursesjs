cbfed923416bd02c7d80b9640b9bc1971f8db19f
Ruslan <pereverzevruslan228@gmail.com>
2021-06-17 17:37:11 +0300

    first work

---
+++ 9.1 pz.js
@@ -0,0 +1,22 @@
+ const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
+const personamlMovieDB = {
+    count:  numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+    b = prompt('На сколько оцените его?', ''),
+    c = prompt('Один из последних просмотренных фильмов?',''),
+    d = prompt('На сколько оцените его?', '');
+
+personamlMovieDB.movies[a] = b;
+personamlMovieDB.movies[c] = d;
+
+console.log(personamlMovieDB);
+
+
